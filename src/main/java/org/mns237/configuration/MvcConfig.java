828474a5680a9897d9e7c46c6e51e49bac2dbd06
Ghislain <gzeleu@outlook.de>
2021-04-30 18:01:59 +0200

    Added yml file for build and test automatically (#25) (#26)
    * Add ymal file for CI (#25)
    
    * Fixe eroro on secirt
    
    * Fixed BscryptPasswordEncoder class
    
    Co-authored-by: Ghislain <Gzeleu@gmx.de>

--- src/main/java/org/mns237/configuration/MvcConfig.java
+++ src/main/java/org/mns237/configuration/MvcConfig.java
@@ -1,13 +1,15 @@
 package org.mns237.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
 
 @Configuration
+
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -23,6 +25,8 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Bean
     public BCryptPasswordEncoder passwordEncoder() {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+
         return bCryptPasswordEncoder;
     }
 	

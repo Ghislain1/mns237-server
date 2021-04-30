52faa0cd1a1430f1084f7c79046361576e87b2b9
Ghislain <Gzeleu@gmx.de>
2021-04-30 17:33:24 +0200

    Fixe eroro on secirt

--- src/main/java/org/mns237/configuration/MvcConfig.java
+++ src/main/java/org/mns237/configuration/MvcConfig.java
@@ -4,10 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
 
 @Configuration
+
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -23,6 +24,8 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Bean
     public BCryptPasswordEncoder passwordEncoder() {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+
         return bCryptPasswordEncoder;
     }
 	

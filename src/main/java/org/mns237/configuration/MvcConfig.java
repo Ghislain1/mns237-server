1992e7afac3671a75b7735aa7aad98045b6fe00c
Ghislain <Gzeleu@gmx.de>
2021-04-30 18:00:51 +0200

    Fixed BscryptPasswordEncoder class

--- src/main/java/org/mns237/configuration/MvcConfig.java
+++ src/main/java/org/mns237/configuration/MvcConfig.java
@@ -1,6 +1,7 @@
 package org.mns237.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.context.annotation.Bean;

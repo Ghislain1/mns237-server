4659ffdc00d8827fad1a20edebbf66a16142edc8
idrice24 <50774032+idrice24@users.noreply.github.com>
2021-05-09 23:55:12 +0100

    Update TopicServiceImpl.java #14

--- src/main/java/org/mns237/service/TopicServiceImpl.java
+++ src/main/java/org/mns237/service/TopicServiceImpl.java
@@ -3,9 +3,11 @@ package org.mns237.service;
 import org.mns237.dao.TopicRepository;
 import org.mns237.entity.Topic;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TopicServiceImpl implements TopicService {
     @Autowired
     private TopicRepository topicRepository;

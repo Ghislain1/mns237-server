9f590cd7d59a5738e6fa871e0574407df335bd3d
idrice24 <50774032+idrice24@users.noreply.github.com>
2021-05-14 01:38:35 +0100

    Update TopicController.java #14

--- src/main/java/org/mns237/controller/TopicController.java
+++ src/main/java/org/mns237/controller/TopicController.java
@@ -3,10 +3,13 @@ package org.mns237.controller;
 
 import org.mns237.dao.TopicRepository;
 import org.mns237.dao.CommentRepository;
+import org.mns237.service.CommentService;
 import org.mns237.service.TopicService;
 import org.mns237.entity.Comments;
 import org.mns237.entity.Topic;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,13 +21,15 @@ public class TopicController {
     @Autowired
     private final TopicRepository topicRepository;
     private final TopicService topicService;
+    private final CommentService commentService;
     private final CommentRepository commentRepository;
 
 
-    public TopicController(TopicRepository topicRepository, CommentRepository commentRepository, TopicService topicService) {
+    public TopicController(TopicRepository topicRepository, CommentService commentService, TopicService topicService, CommentRepository commentRepository) {
         this.topicRepository = topicRepository;
-        this.commentRepository = commentRepository;
         this.topicService = topicService;
+        this.commentService = commentService;
+        this.commentRepository = commentRepository;
     }
 
     // get all blogs from DB
@@ -44,10 +49,42 @@ public class TopicController {
         topicRepository.save(blog);
     }
     // save Comments in DB
-    @PostMapping("/Comments/blog")
+    @PostMapping("/comments/blog")
     public void addComment(@RequestBody Comments comments){
         commentRepository.save(comments);
     }
+
+    // edit product from DB
+    @GetMapping("admin/topics/edit/{id}")
+    public String showUpdate(@PathVariable("id") long id){
+        try {
+            Topic topics = topicService.getTopicsById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "you are editing the blog with id : "+ id+"........";
+    }
+
+    // update blog from db
+    @PostMapping("/admin/blogs/update/{id}")
+    public String updateTopic(@PathVariable("id") long id, Topic topics, BindingResult result, Model model){
+        if (result.hasErrors()){
+            topics.setId(id);
+            return "wrong id";
+        }
+        topicRepository.save(topics);
+        model.addAttribute("topics", topicRepository.findAll());
+        return "you have update the topic with id "+ id;
+    }
     // delete blog from DB
-    // edit blog from DB
+    @GetMapping("/admin/topic/delete/{id}")
+    public String deleteTopic(@PathVariable("id") long id, Model model){
+        Topic topics = topicRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("invalid topic id:" + id));
+        topicRepository.delete(topics);
+        model.addAttribute("topic", topicRepository.findAll());
+        System.out.print("you have enter a wrong id: "+ id);
+        return "you have delete the topic with id: "+ id;
+    }
+
 }

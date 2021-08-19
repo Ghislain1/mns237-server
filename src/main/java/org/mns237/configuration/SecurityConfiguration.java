e0cdce005c616a9b5fc03998b5982579c8ff6094
idrice24 <50774032+idrice24@users.noreply.github.com>
2021-08-19 15:59:36 +0100

    update security files #34
    this is add options to encrypt password and add login and logout function in the project

--- src/main/java/org/mns237/configuration/SecurityConfiguration.java
+++ src/main/java/org/mns237/configuration/SecurityConfiguration.java
@@ -1,5 +1,73 @@
 package org.mns237.configuration;
 
-public class SecurityConfiguration{
-	
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	@Autowired
+	private DataSource dataSource;
+
+	@Value("${spring.queries.users-query}")
+	private String usersQuery;
+
+	@Value("${spring.queries.roles-query}")
+	private String rolesQuery;
+
+	public SecurityConfiguration(BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
+
+	@Override
+	protected void configure (AuthenticationManagerBuilder auth)
+	throws Exception {
+		auth.
+				jdbcAuthentication()
+		.usersByUsernameQuery(usersQuery)
+		.authoritiesByUsernameQuery(rolesQuery)
+		.dataSource(dataSource)
+		.passwordEncoder(bCryptPasswordEncoder);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.
+			authorizeRequests()
+			.antMatchers("/api/blogs").permitAll()
+			.antMatchers("/api/video/list").permitAll()
+			.antMatchers("/contact").permitAll()
+			.antMatchers("/media").permitAll()
+			.antMatchers("/api/home").permitAll()
+			.antMatchers("/products").permitAll()
+			.antMatchers("/api/register").permitAll()
+			.antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
+			.authenticated().and().csrf().disable().formLogin()
+			.loginPage("/").failureUrl("/login?error=true")
+			.defaultSuccessUrl("/admin/dashboard")
+			.usernameParameter("username")
+			.passwordParameter("password")
+			.and().logout();
+
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web
+			.ignoring()
+			.antMatchers("/");
+	}
+
 }

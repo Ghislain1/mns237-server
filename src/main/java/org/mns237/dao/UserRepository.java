3511f61a3c13bc560ff3dd251cb91b4c0239ee7e
idrice24 <idrice.tsafouet@yahoo.com>
2022-09-10 21:32:10 +0100

    Update UserRepository.java

--- src/main/java/org/mns237/dao/UserRepository.java
+++ src/main/java/org/mns237/dao/UserRepository.java
@@ -3,6 +3,7 @@ package org.mns237.dao;
 import org.mns237.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository("userRepository")
 public interface UserRepository extends JpaRepository<Users, Long> {

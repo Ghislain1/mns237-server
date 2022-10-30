e571a246151b71783e0d8e42a7b54c094c5af52a
idrice24 <idrice.tsafouet@yahoo.com>
2022-10-30 18:00:33 +0100

    update files
    ** update data to take role entity
    ** create the role entity

--- src/main/java/org/mns237/entity/Role.java
+++ src/main/java/org/mns237/entity/Role.java
@@ -8,7 +8,7 @@ public class Role{
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "role_id")
-	private int id;
+	private int role_id;
 	@Column(name = "role")
 	private String role;
 
